cb5dd58153cd2d89c409d6fcd0909c3b6d5934dc
Tiago Cordeiro <tiago@mulhergorila.com>
2019-02-22 15:04:47 -0300

    Configura UserProfile.
    Configura requirements-dev.
    Configura testes básicos.
    Configura coverage.
    Aplica migrações.

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory, Client
 
-# Create your tests here.
+from .views import index
+
+
+class WebsiteHomeTestCase(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.client = Client()
+
+    def test_status_code_is_200(self):
+        request = self.factory.get('/')
+
+        response = index(request)
+        self.assertEqual(response.status_code, 200)
