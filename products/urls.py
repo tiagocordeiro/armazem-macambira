f242da70bcdadd55a482733628ad496ee243e0ab
Tiago Cordeiro <tiago@mulhergorila.com>
2019-04-14 12:57:09 -0300

    SetUp some templates.
    This close #22

--- products/urls.py
+++ products/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('produtos/', views.product_list, name='product_list'),
     path('produto/<slug>/', views.product_detail, name='product_detail'),
+    path('categorias/', views.categories_list, name='categories_list'),
     path('<slug>/', views.category_list, name='category_list'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from products.facade import get_product, get_products_category
 
+from products.facade import get_all_categories
+
 
 def product_list(request):
     return render(request, 'products/product_list.html')
@@ -8,13 +10,25 @@ def product_list(request):
 
 def product_detail(request, slug):
     produto = get_product(slug)
+    categorias = get_all_categories()
     categoria = produto.category.slug
     ctx = {'produto': produto,
-           'categoria': categoria}
+           'categoria': categoria,
+           'categorias': categorias}
     return render(request, 'products/product_detail.html', context=ctx)
 
 
 def category_list(request, slug):
     produtos_categoria = get_products_category(slug)
-    ctx = {'produtos_categoria': produtos_categoria, }
+    categorias = get_all_categories()
+    categoria = slug
+    ctx = {'produtos_categoria': produtos_categoria,
+           'categorias': categorias,
+           'categoria': categoria}
     return render(request, 'products/category_list.html', context=ctx)
+
+
+def categories_list(request):
+    categorias = get_all_categories()
+    ctx = {'categorias': categorias, }
+    return render(request, 'products/categories.html', context=ctx)
